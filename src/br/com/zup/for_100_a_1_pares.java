a40c956fd9dae3ee5a811ece4c2e257c25f01e67
Raquel Leão <vc.raquel@gmail.com>
2021-09-02 21:20:16 -0300

    exercicio de 100 a 1 pares

---
+++ src/br/com/zup/for_100_a_1_pares.java
@@ -0,0 +1,11 @@
+package br.com.zup;
+
+public class for_100_a_1_pares {
+    public static void main(String[] args) {
+        for (int cont = 100; cont >= 1; cont -= 1) {
+            if (cont % 2 == 0) {
+                System.out.println(cont);
+            }
+        }
+    }
+}
