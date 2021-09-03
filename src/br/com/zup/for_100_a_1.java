c79d5f75cecb722e55c90393da018c8221140399
Raquel Leão <vc.raquel@gmail.com>
2021-09-02 21:14:34 -0300

    exercicio de 100 a 1

---
+++ src/br/com/zup/for_100_a_1.java
@@ -0,0 +1,9 @@
+package br.com.zup;
+
+public class for_100_a_1 {
+    public static void main(String[] args) {
+        for (int cont = 100; cont >= 1; cont -= 1){
+            System.out.println(cont);
+        }
+    }
+}
